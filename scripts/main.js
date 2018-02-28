423d05db93eeba5523f9b5841b0a981bd3e893a8
Gavin Look <GTLook@gmail.com>
2018-02-28 12:50:11 -0800

    completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,32 @@
 console.log('Welcome to Validation Station!')
 console.log('NEW TRAIN CAR FORM:', document.querySelector('#new-train-car-form'))
+
+const form = document.querySelector('#new-train-car-form')
+form.addEventListener('submit',(event)=>{
+  event.preventDefault()
+
+
+  let trainKind = document.querySelector('#train-kind').value
+  if(trainKind == 'passenger' || trainKind == 'observation'){
+
+    const seatNumber = document.querySelector('#train-seats').value
+    if(seatNumber){
+    //value.customValidity("Please Select Number of Seats")
+    }
+    else{
+      console.log('error')
+    }
+  }
+})
+
+
+form.addEventListener('change',(event)=>{
+  event.preventDefault()
+  let trainKind = document.querySelector('#train-kind').value
+  if(trainKind == 'passenger' || trainKind == 'observation'){
+  document.querySelector('.seatHidden').classList.remove('d-none')
+  }
+  else{
+    document.querySelector('.seatHidden').classList.add('d-none')
+  }
+})
